fe29adc9233077e8ee33f0c9b169d0b5f475ce9b
James Hughes <jhughes1010@gmail.com>
2011-01-30 20:20:36 -0700

    Commit the rails shell
    Signed-off-by: James Hughes <jhughes1010@gmail.com>

---
+++ resource-tracker/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResourceTracker::Application.config.secret_token = 'a6d4615ca27baee7b7e4f10d2046c11b7e0b4fa988a6b6163b0f77fcc6447b5e855158a64e62ecea142c38bcf1987b1eb1d48182f2cc076d8f4f1dc3682fd008'
